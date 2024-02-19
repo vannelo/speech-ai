82267ec1fe01bd2a54ceadd4fa9050c7220e75d6
Vannelo <work@vannelo.com>
2024-02-19 11:31:36 -0600

    work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-DXEZYQXJD4" />
     </html>
   );
 }
